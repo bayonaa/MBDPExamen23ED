3e417bb060afa0582433a33e6712bb79a055edfc
Miguel Bayona <mbayona03@gmail.com>
2023-12-05 13:34:54 +0100

    MostrarNumeros.java inservible.

--- MostrarNumeros.java
+++ MostrarNumeros.java
@@ -2,21 +2,11 @@ import java.util.Scanner;
 
 public class MostrarNumeros {
 
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Por favor, ingresa el primer número:");
-
-        double numero1 = scanner.nextDouble();
-
-        System.out.println("Ahora, ingresa el segundo número:");
-
-        double numero2 = scanner.nextDouble();
-
+    public st
+        Scanner scanner =
         System.out.println("Los números ingresados son:");
 
-        System.out.println("Número 1: " + numero1);
+        System.out.print1: " + numero1);
 
         System.out.println("Número 2: " + numero2);
 
@@ -25,15 +15,13 @@ public class MostrarNumeros {
 <<<<<<< HEAD
 
     public static double sumar(double a, double b) {
-        return a + b;
+        retur
     }
     public static double multiplicar(double a, double b) {
         return a * b;
     }
 
-    public static double potencia(double base, double exponente) {
-        return Math.pow(base, exponente);
-=======
+    public static double p
     
     public static double sumar(double a, double b) {
 
@@ -46,13 +34,11 @@ public class MostrarNumeros {
 
     }
 
-    public static double potencia(double base, double exponente) {
-
-        return Math.pow(base, exponente);
+    public static doublxponente);
 
     }
 
-    public static double restar(double a, double b) {
+    public statictar(double a, double b) {
 
         return a - b;
 
@@ -66,7 +52,7 @@ public class MostrarNumeros {
 
         } else {
 
-            throw new IllegalArgumentException("No se puede dividir por cero");
+            throw new Illegaltion("No se puede dividir por cero");
 
         }
 
@@ -80,10 +66,8 @@ public class MostrarNumeros {
 
         } else {
 
-            throw new IllegalArgumentException("No se puede calcular la raíz cuadrada de un número negativo");
+            throw new ion("No se puede calcular la raíivo");
 
-        }
 
->>>>>>> RESTADORES
     }
 }
